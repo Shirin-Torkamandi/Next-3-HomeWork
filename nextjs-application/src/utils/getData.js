686c80cdd63dfd2b59493fb501b803ff085b9b03
Shirin-Torkamandi <shirin.t44@gmail.com>
2024-09-20 00:34:47 +0330

    changing the fetch method to have limit of 8

--- nextjs-application/src/utils/getData.js
+++ nextjs-application/src/utils/getData.js
@@ -1,6 +1,6 @@
 export default async function getData(url) {
     try {
-      const response = await fetch(url);
+      const response = await fetch(`${url}?limit=8`);
       const data = await response.json();
       return data;
     } catch (error) {
